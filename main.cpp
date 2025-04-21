1415c79bd8d7f0479c2b80fd980c79a779c92414
guwoh <143280760+guwoh@users.noreply.github.com>
2025-04-22 01:23:02 +0700

    push 0

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-04-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
